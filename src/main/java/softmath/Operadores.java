97a7e2dafd6bb2f1925f913cce6ef89ffb17e036
sirlocust <jua56yu@hotmail.com>
2021-09-01 14:42:50 -0500

    creando el metodo raiz cuadrada

--- src/main/java/softmath/Operadores.java
+++ src/main/java/softmath/Operadores.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 
 public class Operadores {
 
-    public static double crearPotencia(double base, double exponente ){
-        return Math.pow(base,exponente);
+    public static double crearPotencia(double base, double exponente) {
+        return Math.pow(base, exponente);
+    }
+
+    public static double raizCuadrada(double radical, double radicando) {
+        return 0;
     }
 
 }
