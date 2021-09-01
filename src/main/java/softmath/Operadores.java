ae38337e6a7d43a18be03812907226f85040d3dd
Jhon Serna <jhonalfaber@gmail.com>
2021-09-01 16:39:39 -0500

    Solucion error de merge en Operadores.java.

--- src/main/java/softmath/Operadores.java
+++ src/main/java/softmath/Operadores.java
@@ -9,8 +9,9 @@ public class Operadores {
     }
 
 
-    public double resta(double nro1, double nro2){
+    public double resta(double nro1, double nro2) {
         return (nro1 - nro2);
+    }
 
     public static double crearCoseno(double angulo){
         return Math.cos(Math.toRadians(angulo));
