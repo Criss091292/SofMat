71e83a4b7400daa63639caa1df4741fbdac6ed85
mvm11 <mvalenciaminota@gmail.com>
2021-09-01 15:10:58 -0500

    Se agrega la función de la división

--- src/main/java/softmath/Operadores.java
+++ src/main/java/softmath/Operadores.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class Operadores {
 
+    // método para dividir
+    public void division(double num1, double num2){
+        System.out.println("El resultado de la división es: " + num1/num2);
+    }
+
     public static double crearPotencia(double base, double exponente ){
         return Math.pow(base,exponente);
     }
