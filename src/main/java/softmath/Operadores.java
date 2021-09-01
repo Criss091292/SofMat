69f6c2f25be4c41fdbda5f0a6ddb5584fa19d08c
Daniel <jdaniel.gonzalez@udea.edu.co>
2021-09-01 15:21:02 -0500

    Se agrego la validacion de la division

--- src/main/java/softmath/Operadores.java
+++ src/main/java/softmath/Operadores.java
@@ -4,9 +4,17 @@ import java.util.Scanner;
 
 public class Operadores {
 
-    // método para dividir
     public void division(double num1, double num2){
-        System.out.println("El resultado de la división es: " + num1/num2);
+        //validacion division entre cero
+        if(num2==0)
+        {
+            System.out.println("No se puede dividir entre cero");
+        }
+        // método para dividir
+        if(num2!=0)
+        {
+            System.out.println("El resultado de la división es: " + num1/num2);
+        }
     }
 
     public static double crearPotencia(double base, double exponente ){
