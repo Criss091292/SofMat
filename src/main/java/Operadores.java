4b46042f0725a7873b6680b88defaee13bf19cad
Daniel <jdaniel.gonzalez@udea.edu.co>
2021-09-01 14:24:58 -0500

    Se hace la validacion de la division

--- src/main/java/Operadores.java
+++ src/main/java/Operadores.java
@@ -1,7 +1,18 @@
 
 public class Operadores {
 
-    public double division(double num1, double num2){
-        return num1/num2;
+    public void division(double num1, double num2){
+
+
+        if(num2==0)
+        {
+            System.out.println("No se puede dividir entre cero.");
+        }
+
+        if(num2!=0)
+        {
+            System.out.println("El Resutado de dividir es: "+num1/num2);
+        }
+
     }
 }
