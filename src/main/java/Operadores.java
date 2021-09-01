3d37d0743bc027936830aba6fa9d29d2a4b4106e
Diego Martinez <juandiego337@hotmail.com>
2021-09-01 13:39:03 -0500

    Metodo generado para mostrar la potencia de un numero

--- src/main/java/Operadores.java
+++ src/main/java/Operadores.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class Operadores {
 
+    public static double crearPotencia(double base, double exponente ){
+        return Math.pow(base,exponente);
+    }
 
 }
